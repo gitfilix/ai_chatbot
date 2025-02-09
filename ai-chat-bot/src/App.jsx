3fa03dc28d4ece5846a12b13abd4414dd02f333e
gitfilix <runlevel07@gmail.com>
2025-02-09 13:05:51 +0100

    AI-Bot: make chatinput fields and response dynamic with date unique key

--- ai-chat-bot/src/App.jsx
+++ ai-chat-bot/src/App.jsx
@@ -3,13 +3,24 @@ import ChatBotStart from "./Components/ChatBotStart"
 import ChatBotApp from "./Components/ChatBotApp"
 
 const App = () => {
+  // chat state: switching view between chat start and chat app
   const [isChatting, setIsChatting] = useState(false)
+  // chat state handle
+  const [chats, setChats] = useState([])
 
   const handleStartChat = () => {
     // lets wait for 250ms before we start chatting
     setTimeout(() => {
       setIsChatting(true)
     }, 250)
+
+    if (chats.length === 0) {
+      const newChat = {
+        id: `Chat ${new Date().toLocaleStringDateString('de-DE')} ${new Date().toLocaleTimeString()}`,
+        messages: [],
+      }
+      setChats([newChat])
+    }
   }
 
   const handleGoBack = () => {
@@ -19,7 +30,11 @@ const App = () => {
   return (
     <div className='container'>
       {isChatting 
-      ? (<ChatBotApp onGoBack={handleGoBack} />) 
+      ? (<ChatBotApp 
+          onGoBack={handleGoBack}
+          chats={chats}
+          setChats={setChats}
+          />) 
       : (<ChatBotStart onStartChat={handleStartChat} />)
     }
     </div>
--- ai-chat-bot/src/Components/ChatBotApp.jsx
+++ ai-chat-bot/src/Components/ChatBotApp.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ChatBotApp.css'
 
-const ChatBotApp = ({ onGoBack }) => {
+const ChatBotApp = ({ onGoBack, chats, setChats }) => {
+  console.log('chats', chats)
+  const [inputValue, setInputValue] = useState('')
+  const [messages, setMessages] = useState(chats[0]?.messages || [])
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const sendMessage = () => {
+    if(inputValue.trim() === '') return
+    
+    // create a new message
+    const newMessage = {
+      type: 'prompt',
+      text: inputValue,
+      timestamp: new Date().toLocaleString()
+    }
+
+    // update the messages with the new message
+    const updatedMessages = [...messages, newMessage]
+    setMessages(updatedMessages)
+    
+    // clear the input field by setting the input field value to an empty string
+    setInputValue('')
+
+    // updatedChats: update the chats with the new messages, if the chat is the first chat
+    // in the chats array, update the messages with the updated messages
+    const updatedChats = chats.map((chat, index) => {
+      // if the chat is the first chat in the chats array we update the messages
+      if (index === 0) {
+        return {
+          ...chat,
+          messages: updatedMessages
+        }
+      }
+      // otherwise we return the chat as it is
+      return chat
+    })
+    setChats(updatedChats)
+  }
+
+  // keyboard event listener for enter key
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      sendMessage()
+    }
+  }
+
   return (
     <div className='chat-app'>
       <div className='chat-list'>
@@ -9,18 +58,14 @@ const ChatBotApp = ({ onGoBack }) => {
           <h2>Chat List</h2>
           <i className='bx bx-edit-alt new-chat'></i>
         </div>
-        <div className='chat-list-item active'>
-          <h4>chat 20/07/2025 12:43:43 PM</h4>
-          <i className='bx bx-x-circle'></i>
-        </div>
-        <div className='chat-list-item'>
-          <h4>chat 20/07/2025 12:43:43 PM</h4>
-          <i className='bx bx-x-circle'></i>
-        </div>
-        <div className='chat-list-item'>
-          <h4>chat 20/07/2025 12:43:43 PM</h4>
-          <i className='bx bx-x-circle'></i>
-        </div>
+        {chats.map((chat, index) => (
+            <div 
+              key={chat.id} 
+              className={`chat-list-item ${index === 0 ? 'active' : ''}`}>
+              <h4>Chat Id:{chat.id}</h4>
+              <i className='bx bx-x-circle'></i>
+            </div>
+        ))}
       </div>
       <div className='chat-window'>
         <div className='chat-title'>
@@ -30,20 +75,34 @@ const ChatBotApp = ({ onGoBack }) => {
           </i>
         </div>
         <div className='chat'>
-          <div className='prompt'>
-            <p>Hi, I am AI chatbot. How can I help you today?</p>
-            <span>20/07/2025 12:43:43 PM</span>
-          </div>
+            {messages.map((msg, index) => (
+              <div key={index} className={msg.type === 'prompt' ? 'prompt' : 'response'}>
+                <p>{msg.text}</p>
+                <span>{msg.timestamp}</span>
+              </div>
+            ))}
           <div className='response'>
             <p>Hello, I am just a computer programm but I can assist you. how can I help you?</p>
             <span>20/07/2025 12:43:43 PM</span>
           </div>
           <div className="typing">Typing... </div>
         </div>
-        <form className='msg-form'>
+        <form className='msg-form'
+          onSubmit={(e) => {
+            e.preventDefault()
+            sendMessage()
+          }}>
           <i className='fa-solid  fa-face-smile emoji'></i>
-          <input type='text' placeholder='Ask me anything..' className='msg-input' />
-          <i className='fa-solid fa-paper-plane'></i>
+          <input 
+            type='text' 
+            placeholder='Ask me anything... FLX chatbot may help you - or not'  
+            className='msg-input' 
+            value={inputValue} 
+            onChange={handleInputChange} />
+          <i 
+            className='fa-solid fa-paper-plane' 
+            onClick={sendMessage}>
+            </i>
         </form>
       </div>
     </div>
