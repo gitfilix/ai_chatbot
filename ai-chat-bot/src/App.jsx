08320d887c5d0b63a16a5b7f275598118deef0dc
gitfilix <runlevel07@gmail.com>
2025-02-09 11:33:59 +0100

    AI-Bot: switching views with a simple useState hook.

--- ai-chat-bot/src/App.jsx
+++ ai-chat-bot/src/App.jsx
@@ -1,12 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import ChatBotStart from "./Components/ChatBotStart"
 import ChatBotApp from "./Components/ChatBotApp"
 
 const App = () => {
+  const [isChatting, setIsChatting] = useState(false)
+
+  const handleStartChat = () => {
+    // lets wait for 250ms before we start chatting
+    setTimeout(() => {
+      setIsChatting(true)
+    }, 250)
+  }
+
+  const handleGoBack = () => {
+    setIsChatting(false)
+  }
+
   return (
     <div className='container'>
-      <ChatBotApp />
-      {/* <ChatBotStart /> */}
+      {isChatting 
+      ? (<ChatBotApp onGoBack={handleGoBack} />) 
+      : (<ChatBotStart onStartChat={handleStartChat} />)
+    }
     </div>
   )
 }
--- ai-chat-bot/src/Components/ChatBotStart.jsx
+++ ai-chat-bot/src/Components/ChatBotStart.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './ChatBotStart.css'
 
-const ChatBotStart = () => {
-    return (
-        <div className='start-page'>
-            <button className='start-page-btn'>Chat with AI</button>
-            ChatBot
-        </div>
-    );
+const ChatBotStart = ({ onStartChat }) => {
+
+  return (
+    <div className='start-page'>
+      <button className='start-page-btn' onClick={onStartChat}>Chat with AI</button>
+    </div>
+  );
 };
 
 export default ChatBotStart;
